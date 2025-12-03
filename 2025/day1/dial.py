a356f89c372b2640ebd0d6268957c8af687a6217
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2025-12-03 08:58:36 +0000

    Update viz.

--- 2025/day1/dial.py
+++ 2025/day1/dial.py
@@ -1,5 +1,5 @@
 # /// script
-# requires-python = ">=3.14"
+# requires-python = "==3.12"
 # dependencies = [
 #     "pygame",
 # ]
@@ -23,6 +23,9 @@ BLACK = (16, 15, 15)
 PURPLE = (139, 126, 200)
 FPS = 10
 
+pygame.font.init()
+FONT = pygame.font.Font(size=70)
+
 rotations = []
 with open("input.txt", "r") as f:
     for line in f:
@@ -35,6 +38,7 @@ with open("input.txt", "r") as f:
         )
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
 screen.fill(WHITE)
 pygame.display.flip()
 
@@ -47,9 +51,19 @@ def draw_dial():
         (WIDTH // 2, HEIGHT // 2),
         RADIUS,
     )
+    tick_x = WIDTH // 2
+    tick_y = HEIGHT // 2 - RADIUS
+    pygame.draw.line(
+        screen,
+        BLACK,
+        (tick_x, tick_y - 10),
+        (tick_x, tick_y + 10),
+        width=7,
+    )
 
 def draw_pointer(position):
-    angle = position * (2 * math.pi / 100)
+    # `position - 25` moves the 0 to the top of the dial.
+    angle = (position - 25) * (2 * math.pi / 100)
     offset_x = RADIUS * math.cos(angle)
     offset_y = RADIUS * math.sin(angle)
     end_point = (
@@ -65,18 +79,37 @@ def draw_pointer(position):
         width=5,
     )
 
+
+# Writes the part 1 and part 2 scores on the screen.
+def draw_scores():
+    rendered_text = FONT.render(str(part1_score), 1, BLACK)
+    text_width, text_height = rendered_text.get_size()
+    screen.blit(
+        rendered_text,
+        (
+            10,
+            HEIGHT - text_height,
+        ),
+    )
+
 # Draw the frames of the pointer moving.
 def animate_pointer(start, move):
+    global part1_score
+
     direction = -1 if move < 0 else 1
     for tick in range(1, abs(move) + 1):
         draw_dial()
         position = (start + tick * direction) % 100
+        if position == 0:
+            part1_score += 1
         draw_pointer(position)
+        draw_scores()
         yield  # Give pygame the opportunity to update the screen.
     # Here, we're done.
 
 clock = pygame.time.Clock()
 
+part1_score = 0
 position = 50
 current_animation = None
 current_rotation_index = 0
@@ -101,4 +134,5 @@ while True:
         position += rotation
         position %= 100
         current_animation = None
+
     pygame.display.flip()
